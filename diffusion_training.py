187068ce14bd01ad9ba0ce9046b543066852f7df
zer0 <zer0@zer0s-MacBook-Pro.local>
2022-12-21 17:13:31 +0800

    add basic codes

--- diffusion_training.py
+++ diffusion_training.py
@@ -9,29 +9,46 @@ import random
 import sys
 import torch
 from torchvision import transforms
+from torch.utils.tensorboard import SummaryWriter
+from tqdm.auto import tqdm
+from diffusers import UNet2DModel
 
-from utils import load_parameters
+import utils
 
 
 # set environment configuration
 os.environ["CUDA_VISIBLE_DEVICES"] = '0'
 
+def training(args):
+    #basic configuration
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    for folder in ["metric", "images", "model"]:
+        f_dir = os.path.join(args["output_dir"], folder)
+        utils.create_folders(f_dir)
+    
+    del folder, f_dir
+
+    #initial models
+    writer = SummaryWriter()
+
+    model = UNet2DModel(
+        sample_size=args["resolution"],
+        in_channels=args["in_channels"],
+        out_channels=args["in_channels"]
+
+    )
+
+    
+
+    
+
 # TODO: Using DDIM instead of DDPM
-
-
 def ddim_ano(args):
     """
     Using DDIM to perform anomaly detection
     """
-
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-    # set in_channel
-
-
-    # model setting
-
-
+    pass
 
 # TODO: Using DDPM with X_T to X_{T-K}
 def partly_ddpm_ano():
@@ -45,13 +62,15 @@ if __name__ == '__main__':
     
     cfg = sys.argv[1]
     if cfg.isnumeric():
-        para_dir = 'config{}'.format(cfg)
+        para_name = 'configs{}'.format(cfg)
     elif cfg.endswith('.json'):
-        para_dir = cfg
+        para_name = cfg
     else:
-        para_dir = cfg + ".json"
-
+        para_name = cfg + ".json"
+    para_dir = os.path.join(config_dir, para_name)
+    
     # parse input
-    args = load_parameters(para_dir)
+    args = utils.load_parameters(para_dir)
 
-    ddim_ano(args)
+    training(args)
+
--- utils.py
+++ utils.py
@@ -1,18 +1,57 @@
 import json
+import os
+from collections import defaultdict
+
 
 def load_parameters(para_dir:str) -> dict:
     """
     loading configure json file.
     path of json file folder:./configs/
     """
-    with open(para_dir, 'r') as f:
-        args = json.load(f)
 
+    cfgs_name = os.path.basename(para_dir)[:-5]
+    print("configurations:"+cfgs_name)
+    with open(para_dir, 'r') as f:
+        args_dict = json.load(f)
+
+    args = defaultdict(str)
+    args.update(args_dict)
+
+    args["cfgs_name"] = cfgs_name
+
+    def set_default_value(pairs):
+    #pairs: {(arg_name, default_value)}
+        for arg_name in pairs:
+            if args[arg_name] == '':
+                args[arg_name] = args[arg_name]
+
+    #set input channel
     if args["dataset"].lower() == "leather":
-        in_channels = 3
-    elif args["channels"] != "":
-        in_channels = args["channels"]
-    else:
-        in_channels = 1
+        args["in_channels"] = 3
+    elif args["in_channels"] == '':
+        args["in_channels"] = 1
+
+    #set input path
+    if args["input_path"] == "":
+        if args["dataset"] == "leather":
+            args["input_path"] = os.path.join("~", "Datasets", "leather")
+
+
+    #set output path
+    set_default_value("output_path", os.path.join('.','output', cfgs_name))
+ 
+    #set Unet structure
+    default_Unet = {
+
+    }
+    set_default_value("layers_per_block", 2)
+    set_default_value("block_out_channels", (128, 128, 256, 256, 512, 512))
+    set_default_value()
 
     return args
+
+
+
+def create_folders(f_dir):
+    if not os.path.exists(f_dir):
+        os.makedirs(f_dir)
