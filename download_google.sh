5bae47380a464a3f8c362c64169ea97aa3bcaa7a
Zer0-00 <linxuehong1@gmail.com>
2023-01-25 20:10:59 -0700

    update download script

--- download_google.sh
+++ download_google.sh
@@ -4,11 +4,11 @@
 cd ~/Datasets
 
 # Download zip dataset from Google Drive
-filename='CheXpert_Processed.zip'
-fileid='1_gis6csLOcRdtM74vC-xfasuDmiZ43L3'
-wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=${fileid}' -O- | sed -rn 's/.confirm=([0-9A-Za-z_]+)./\1\n/p')&id=${fileid}" -O ${filename} && rm -rf /tmp/cookies.txt
+filename='CheXpert_Processed.tar'
+fileid='1xVOF8zwiu0unyFGCDSroXak1X3fb2qJL'
+wget -c --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=${fileid}' -O- | sed -rn 's/.confirm=([0-9A-Za-z_]+)./\1\n/p')&id=${fileid}" -O ${filename} && rm -rf /tmp/cookies.txt
 
-# Unzip
-unzip -q ${filename}
+#Unzip
+tar -xf ${filename}
 rm ${filename}
 cd
