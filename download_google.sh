f6df1a8cda863631e85638fb2804da78c0e95c15
Zer0-00 <linxuehong1@gmail.com>
2023-01-25 22:20:44 -0700

    update runable version for CheXpert

--- download_google.sh
+++ download_google.sh
@@ -4,11 +4,11 @@
 cd ~/Datasets
 
 # Download zip dataset from Google Drive
-filename='CheXpert_Processed.tar'
-fileid='1xVOF8zwiu0unyFGCDSroXak1X3fb2qJL'
+filename='healthy.tar.gz'
+fileid='1ddpJWNUhXRyRLHblq3890pemwmNtyuDO'
 wget -c --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=${fileid}' -O- | sed -rn 's/.confirm=([0-9A-Za-z_]+)./\1\n/p')&id=${fileid}" -O ${filename} && rm -rf /tmp/cookies.txt
 
 #Unzip
-tar -xf ${filename}
+tar -zxvf ${filename}
 # rm ${filename}
 cd
